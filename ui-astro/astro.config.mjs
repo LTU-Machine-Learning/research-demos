3dbac1dc2c1e518b7879594b6d7820d3214080cb
Tom Burellier <tom.burellier@associated.ltu.se>
2025-12-13 03:03:48 +0100

    last try for this night btw"

--- ui-astro/astro.config.mjs
+++ ui-astro/astro.config.mjs
@@ -1,22 +1,18 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-import mdx from '@astrojs/mdx';
-import astroExpressiveCode from 'astro-expressive-code';
 
 export default defineConfig({
-  server: { port: 4321, host: true },
   output: 'static',
-  vite: { build: { assetsInlineLimit: 0 } },
-
   integrations: [
-    astroExpressiveCode(), // must be before mdx()
-    mdx(),
     starlight({
-      title: 'Vision Hub Documentation',
+      title: 'Vision Hub Docs',
+      defaultLocale: 'en',
       sidebar: [
         {
-          label: 'Overview',
-          items: [{ label: 'Introduction', link: '/docs/introduction/' }],
+          label: 'Getting Started',
+          items: [
+            { label: 'Introduction', slug: 'introduction' },
+          ],
         },
       ],
     }),

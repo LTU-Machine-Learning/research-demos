090dcec91b1db49956091ca1b38ca25070a85ffe
Tom Burellier <tom.burellier@associated.ltu.se>
2025-12-13 01:58:53 +0100

    adding documentation page : start

--- ui-astro/astro.config.mjs
+++ ui-astro/astro.config.mjs
@@ -1,15 +1,24 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
 
 export default defineConfig({
   server: { 
     port: 4321, 
     host: true 
   },
-  output: 'static',  
+
+  output: 'static',
+
   vite: {
     build: {
       assetsInlineLimit: 0,
     },
   },
+
+  integrations: [
+    starlight({
+      title: 'Vision Hub Documentation',
+    }),
+  ],
 });

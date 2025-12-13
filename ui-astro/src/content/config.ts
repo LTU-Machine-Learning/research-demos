c96fb74b02dca9413319bba324a32660dd2e2620
Tom Burellier <tom.burellier@associated.ltu.se>
2025-12-13 03:09:23 +0100

    last try for this night btw xd x3"

--- ui-astro/src/content/config.ts
+++ ui-astro/src/content/config.ts
@@ -1,10 +1,12 @@
 import { defineCollection, z } from 'astro:content';
 
-export const collections = {
-  docs: defineCollection({
-    schema: z.object({
-      title: z.string(),
-      description: z.string().optional(),
-    }),
+const docs = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string().optional(),
   }),
+});
+
+export default {
+  docs,
 };
